ade40f860658c36bf595fe346a3117039e29a2e4
ProGamerGov <ProGamerGov@users.noreply.github.com>
2016-10-16 14:58:46 -0600

    Update get_models.sh

--- get_models.sh
+++ get_models.sh
@@ -4,8 +4,10 @@ BVLC='http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel'
 PLACES205='https://s3-us-west-2.amazonaws.com/crowsonkb-deep-dream/googlelet_places205_train_iter_2400000.caffemodel'
 PLACES365='http://places2.csail.mit.edu/models_places365/googlenet_places365.caffemodel'
 RESNET50='http://www.deepdetect.com/models/resnet/ResNet-50-model.caffemodel'
+ILLUST2VEC='http://illustration2vec.net/models/illust2vec_ver200.caffemodel'
 
 ( cd bvlc_googlenet && curl -O "$BVLC" )
 ( cd googlenet_places205 && curl -O "$PLACES205" )
 ( cd googlenet_places365 && curl -O "$PLACES365" )
 ( cd resnet && curl -O "$RESNET50" )
+( cd illust2vec && curl -O "$ILLUST2VEC" )
